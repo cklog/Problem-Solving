8c5ddcb76af78404799d9a719e168b1dd1e745a5
cklog <csknowledgelog@gmail.com>
2024-12-15 19:26:29 +0900

    Create moving-average-of-power-consumption.sql

---
+++ SQL/solvesql/moving-average-of-power-consumption.sql
@@ -0,0 +1,30 @@
+SELECT
+  DATETIME(measured_at, '+10 minutes') AS end_at,
+  ROUND(
+    AVG(zone_quads) OVER (
+      ORDER BY
+        measured_at ROWS BETWEEN 5 PRECEDING
+        AND CURRENT ROW
+    ),
+    2
+  ) AS zone_quads,
+  ROUND(
+    AVG(zone_smir) OVER (
+      ORDER BY
+        measured_at ROWS BETWEEN 5 PRECEDING
+        AND CURRENT ROW
+    ),
+    2
+  ) AS zone_smir,
+  ROUND(
+    AVG(zone_boussafou) OVER (
+      ORDER BY
+        measured_at ROWS BETWEEN 5 PRECEDING
+        AND CURRENT ROW
+    ),
+    2
+  ) AS zone_boussafou
+FROM
+  power_consumptions
+WHERE
+  measured_at BETWEEN '2017-01-01 00:00:00' AND '2017-02-01 00:00:00'
